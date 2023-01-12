2f210e5997025fc836fe72ddd03819d76ccd2f53
Dreikerien <119725279+DedParker@users.noreply.github.com>
2023-01-12 16:02:23 +0300

    added cart

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { RegisterComponent } from './register/register.component';
 import {PostsComponent} from './catalog/posts.component';
 import { ProfileComponent } from './profile/profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: PostsComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'cart', component: CartComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
   { path: '**', component: NotFoundComponent },
